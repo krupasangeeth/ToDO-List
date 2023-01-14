be058bf5ed1bb9c0c0082a5278eccd67630f8f0f
krupa <krupasangeeth107@gmail.com>
2023-01-14 19:09:27 +0530

    implementing todo delete feature

--- src/app/list-to-do/list-to-do.component.ts
+++ src/app/list-to-do/list-to-do.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { ToDoDataService } from '../services/DAO/to-do-data.service';
 
 export class ToDo{
   constructor(
     public id : number,
     public description : string,
     public status : boolean,
-    public targetDate : Date
+    public date : Date
   ){
 
   }
@@ -22,19 +23,42 @@ export class ToDo{
 
 export class ListToDoComponent implements OnInit {
 
-  todos = [
+  message : String | undefined;
+  todos : ToDo[] | undefined;
+  // = [
 
-    new ToDo(1,'Reading a Book',false, new Date()),
-    new ToDo(1,'Learn to Dance',false, new Date()),
-    new ToDo(1,'Eating Food',false, new Date())
+  //   new ToDo(1,'Reading a Book',false, new Date()),
+  //   new ToDo(1,'Learn to Dance',false, new Date()),
+  //   new ToDo(1,'Eating Food',false, new Date())
 
-    // {id : 1, description : 'Reading a Book' },
-    // {id : 2, description : 'Learn to Dance'},
-    // {id : 3, description : 'Eating Food'}
-  ];
-  constructor() { }
+  //   // {id : 1, description : 'Reading a Book' },
+  //   // {id : 2, description : 'Learn to Dance'},
+  //   // {id : 3, description : 'Eating Food'}
+  // ];
+  constructor(
+    private todoService : ToDoDataService
+  ) { }
 
   ngOnInit(): void {
+    this.refreshTods();
+  }
+
+  refreshTods(){
+    this.todoService.getTodoData('krupa').subscribe(
+      response=>{
+        console.log(response);
+        this.todos = response;
+    });
+  }
+
+  deleteTodo(id: any){
+    this.todoService.deleteTodo('krupa',id).subscribe(
+      response => {
+        console.log(response);
+        this.message = "Selected TODO is Deleted "
+        this.refreshTods();
+      }
+    )
   }
 
 }
---
+++ src/app/services/DAO/to-do-data.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ToDo } from 'src/app/list-to-do/list-to-do.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ToDoDataService {
+
+  constructor(private http : HttpClient) { }
+
+
+  getTodoData(name: string){
+    return this.http.get<ToDo[]>(`http://localhost:8080/users/${name}/todos`);
+  }
+
+  deleteTodo(name : String, id: any ){
+    return this.http.delete(`http://localhost:8080/users/${name}/todos/${id}`)
+  }
+
+}
